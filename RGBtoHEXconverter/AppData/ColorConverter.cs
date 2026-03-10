f565adeb23580d56ebe0e584e2cba69ea8233397
Aleksey <a987gr@gmail.com>
2026-03-10 09:03:35 +0300

    Добавьте файлы проекта.

---
+++ RGBtoHEXconverter/AppData/ColorConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGBtoHEXconverter.AppData
+{
+    public class ColorConverter
+    {
+        private int _red;
+        private int _green;
+        private int _blue;
+
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+
+        public ColorConverter()
+        {
+            _red = 0;
+            _blue = 0;
+            _green = 0;
+        }
+
+        public ColorConverter(int red, int green, int blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+
+        public string RGBtoHEX()
+        {
+            return $"#{Red:X2}{Green:X2}{Blue:X2}";
+        }
+    }
+}
---
+++ RGBtoHEXconverter/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using RGBtoHEXconverter.AppData;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RGBtoHEXconverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ColorConverter _converter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertColorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int red = Convert.ToInt32(RedValueTb.Text);
+            int green = Convert.ToInt32(GreenValueTb.Text);
+            int blue = Convert.ToInt32(BlueValueTb.Text);
+
+            _converter = new ColorConverter(red,green,blue);
+        }
+    }
+}
